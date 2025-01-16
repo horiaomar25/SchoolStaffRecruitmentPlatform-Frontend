a212da0bb3b67b049256212d1cd0db57df3c8104
horiaomar25 <horiaomar6@gmail.com>
2025-01-16 18:40:23 +0000

    update

--- schoolstaffrecruitment/app/components/ProfileHeader.jsx
+++ schoolstaffrecruitment/app/components/ProfileHeader.jsx
@@ -4,24 +4,13 @@ import useProfileData from "@/CustomHooks/useProfileData";
 
 
 const ProfileHeader = () => {
-  const {data} = useProfileData();
-  console.log(data);
-
+  
   return (
     <>
-      {data.map((profile) => (
-      <div key = {profile.id}>
-        <h1>{profile.firstName}</h1>
-        <h1>{profile.lastName}</h1>
-        
-      </div>
-      ))}
+      
     </>  
   );
   
-  
-
-    
 
   
 }
