05a26a5ca21ca0285ff79acd4efa6515fd0acbad
horiaomar25 <horiaomar6@gmail.com>
2025-01-19 18:23:54 +0000

    added: daisyui added to tailwind config

--- schoolstaffrecruitment/tailwind.config.js
+++ schoolstaffrecruitment/tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from "daisyui"
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,5 +15,9 @@ module.exports = {
     extend: {
     },
   },
-  plugins: [],
-}
+  daisyui: {
+    themes:[],
+   },
+   plugins: [daisyui],
+ };
+ 
