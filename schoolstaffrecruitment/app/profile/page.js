c812be74d1b78f6b0d32d3340de5296845bd664f
horiaomar25 <horiaomar6@gmail.com>
2025-01-27 12:17:55 +0000

    added: update on responsiveness for profile page

--- schoolstaffrecruitment/app/profile/page.js
+++ schoolstaffrecruitment/app/profile/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "../../components/Navigation";
 import ProfileCard from "../../components/ProfileCard";
 import WorkHistoryCard from "../../components/WorkHistoryCard";
@@ -8,22 +8,37 @@ import ProfileDescriptionCard from "@/components/ProfileDescriptionCard";
 import AssignmentCard from "@/components/AssignmentCard";
 
 function Page() {
+  const [showProfileDescription, setShowProfileDescription] = useState(false);
+
+  const toggleProfileDescription = () => {
+    setShowProfileDescription(!showProfileDescription);
+  };
+
   return (
     <>
       <Navigation />
 
       <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-4">
-       
-        <section className="grid justify-center  lg:border lg:border-blue-500 space-y-4">
+        {/* Column 1 */}
+        <section className="grid justify-center lg:border lg:border-blue-500 space-y-4">
           <ProfileCard />
           <FeedbackCard />
         </section>
 
-        <section className="grid justify-center lg:border lg:border-blue-500 space-y-4 h-full lg:h-screen">
-          <ProfileDescriptionCard className=" lg:h-screen" />
+        {/* Column 2 */}
+        <section className="grid justify-center  lg:border lg:border-blue-500 space-y-4 h-full lg:h-screen">
+          <button
+            onClick={toggleProfileDescription}
+            className="btn btn-primary md:hidden lg:hidden"
+          >
+            {showProfileDescription ? "Hide Profile Description" : "Show Profile Description"}
+          </button>
+          {(showProfileDescription || window.innerWidth >= 764) && (
+            <ProfileDescriptionCard  />
+          )}
         </section>
 
-       
+        {/* Column 3 */}
         <section className="grid justify-center lg:border lg:border-blue-500 space-y-4">
           <AssignmentCard />
           <WorkHistoryCard />
@@ -34,3 +49,4 @@ function Page() {
 }
 
 export default Page;
+
