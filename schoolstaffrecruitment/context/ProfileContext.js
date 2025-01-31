2b67c2f4439a2fdf0927af4b2d87e360eddcff3d
horiaomar25 <horiaomar6@gmail.com>
2025-01-31 11:49:06 +0000

    added: update ProfileContext file

--- schoolstaffrecruitment/context/ProfileContext.js
+++ schoolstaffrecruitment/context/ProfileContext.js
@@ -1,3 +1,49 @@
 "use client"
 
 import {createContext, useContext, useState, useEffect} from 'react';
+
+const ProfileContext = createContext();
+
+export const ProfileProvider = ({children}) => {
+    const[profile, setProfile] = useState(null);
+    const[loading, setLoading] = useState(true);
+    const[error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            const token = localStorage.getItem('token');
+
+            if(!token){
+                setError('Not Authenticated');
+                setLoading(false);
+                return;
+            }
+
+            try {
+                const response = await fetch('http://localhost:8080/api/v1/profile/personal', {
+                    method: 'GET',
+                    headers: {
+                        Authorization: 'Bearer ${token}',
+                        "Content-Type": "application/json"
+                        
+                    },
+                });
+
+                if(!response.ok){
+                    throw new Error('Not Authenticated');
+                }
+
+                const result = await response.json();
+                setProfile(result);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+
+        };
+
+    }, []);
+
+
+}
