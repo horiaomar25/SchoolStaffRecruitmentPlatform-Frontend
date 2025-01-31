4910678ef620cf91d850d1732db4003cdfd9daf1
horiaomar25 <horiaomar6@gmail.com>
2025-01-31 14:30:49 +0000

    added: mapping data to Qualification cards

--- schoolstaffrecruitment/components/QualificationCard.jsx
+++ schoolstaffrecruitment/components/QualificationCard.jsx
@@ -4,40 +4,32 @@ import {useProfile} from '../context/ProfileContext';
 const QualificationCard = () => {
 
   const { profile, loading, error } = useProfile();
+
+  if(loading) return <p>Loading...</p>
+
+  if(error) return <p>Error...</p>
   
 
   return (
     <>
 
-    <div className="card w-96 bg-base-100 card-xs shadow-md">
-        <h2 className="text-3xl font-bold text-center">Work History</h2>
+    <h2 className="text-3xl font-bold text-center ">Qualifications</h2>
+    
+    <div className='space-y-4'>
+      {profile.qualifications.map((qualification, index) => (
+        <div className="card w-96 bg-base-100 border border-black card-xs shadow-md " key={index}>
           <div className="card-body">
-              <h2 className="card-title"></h2>
-              <p>School</p>
-              <p>Year Achieved</p>
-
+              <h3 className="card-title">{qualification.qualificationName}</h3>
+              <p className='font-bold '>{qualification.institutionName}</p>
+              <p className='text-lg'>{qualification.yearObtained}</p>
+             
               <div className="justify-end card-actions">
-                 
               </div>
           </div>
-
-          <div className="card-body">
-              <h2 className="card-title">Xsmall Card</h2>
-              <p>School</p>
-              <p>Year Achieved</p>
-
-              <div className="justify-end card-actions">
-                 
-              </div>
-          </div>
-
+        </div>
+      ))}
+           
       </div>
-
-
-      
-
-      
-     
       
       </>
   )
