1c08e1475f8ea4fd5923d74306e11b97490105a2
horiaomar25 <horiaomar6@gmail.com>
2025-03-25 14:07:10 +0000

    update with credentials

--- schoolstaffrecruitment/customhooks/useAuth.jsx
+++ schoolstaffrecruitment/customhooks/useAuth.jsx
@@ -16,7 +16,7 @@ const useAuth = () => {
                 headers: {
                      'Content-Type': 'application/json'
                 },
-               
+               credentials: 'include'
                 
             });
 
